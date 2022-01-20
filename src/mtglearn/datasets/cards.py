8a03e1aafeff9fd855004d9f6776516286280aa9
devin conathan <dconathan@gmail.com>
2022-01-19 21:06:29 -0600

    figuring out load_dataset api

--- src/mtglearn/datasets/cards.py
+++ src/mtglearn/datasets/cards.py
@@ -1,141 +1,154 @@
-from typing import List
+from typing import List, Mapping, Optional
 import json
 from collections import defaultdict
 import random
 import re
-
-import requests
-from datasets.utils.file_utils import cached_path
-from datasets.features import Features
-from datasets import Value, Dataset, Sequence
+import os
+from functools import lru_cache
+from types import MappingProxyType
+import logging
 
 import attrs
 from attrs import define
-
 import cattrs
+from cattrs.gen import make_dict_unstructure_fn, make_dict_structure_fn, override
+import requests
+from datasets.utils.file_utils import cached_path
+from datasets import Features, Value, Dataset, Sequence, load_from_disk
+
+from ..config import MTGLEARN_CACHE_HOME
+from ..card import Card, CardStats
+from .utils import type2features
+
+
+logger = logging.getLogger(__name__)
 
 
 RAW_DATA_URL = "https://mtgjson.com/api/v5/AllPrintings.json"
+CARDS_DATASET_CACHE = os.path.join(MTGLEARN_CACHE_HOME, "cards")
+CARD_STATS_DATASET_CACHE = os.path.join(MTGLEARN_CACHE_HOME, "card_stats")
+
+EXPANSIONS_WITH_STATS = {"VOW"}
+BASIC_LANDS = {"Plains", "Mountain", "Swamp", "Island", "Forest"}
 
 
-def cls2features(cls) -> Features:
-    features = {}
-    for field in attrs.fields(cls):
-        if field.type is str:
-            features[field.name] = Value("string")
-        elif field.type is int:
-            features[field.name] = Value("int32")
-        elif field.type is float:
-            features[field.name] = Value("float32")
-        elif field.type is List[str]:
-            features[field.name] = Sequence(Value("string"))
-        else:
-            raise NotImplementedError(field.type)
-    return Features(**features)
+@lru_cache(2 ** 8)
+def _get_seventeenlands_stats(
+    expansion: str, stats_format: str = "PremierDraft"
+) -> Mapping[str, CardStats]:
+    logger.info(f"getting 17lands stats for {expansion}...")
+    if expansion not in EXPANSIONS_WITH_STATS:
+        return None
+    endpoint = f"https://www.17lands.com/card_ratings/data?expansion={expansion}&format={stats_format}"
+    response = requests.get(endpoint)
+    raw_seventeenlands_stats = response.json()
+    # mapping proxy type is immutable, for cache purposes
+    seventeenlands_stats = cattrs.structure(raw_seventeenlands_stats, List[CardStats])
+    return MappingProxyType({c.name: c for c in seventeenlands_stats})
 
 
-@define
-class Card:
-    name: str = attrs.field(default="")
-    mana_cost: str = attrs.field(default="")
-    mana_value: int = attrs.field(default=None)
-    types: List[str] = attrs.field(default=attrs.Factory(list))
-    expansion: str = attrs.field(default="")
-    rarity: str = attrs.field(default="")
-    text: str = attrs.field(default="")
-    power: str = attrs.field(default="")  # needs to be str because of `*`
-    toughness: str = attrs.field(default="")  # needs to be str because of `*`
-
-    @classmethod
-    def from_dict(cls, d: dict, **kwargs):
-        d.update(kwargs)
-        d["mana_cost"] = d.get("mana_cost", d.get("manaCost", ""))
-        d["mana_value"] = d.get("mana_value", d.get("manaValue"))
-        return cattrs.structure(d, cls)
-
-    def __str__(self):
-        """Canonical string representation of a card:
-
-        field1_name: field1_value | field2_name: field2_value | ..."""
-        fields = []
-        for f in attrs.fields(type(self)):
-            value = getattr(self, f.name)
-            if isinstance(value, list):
-                value = " ".join(value)
-            if value:
-                fields.append((f.name, value))
-        return re.sub(r"\s+", " ", " | ".join(f"{k}: {v}" for k, v in fields))
+def _join_card_with_stats(card):
+    # seveenlands cards are keyed by their front side
+    seventeenlands_key = card["name"].split(" // ")[0]
+    stats = _get_seventeenlands_stats(card["expansion"])[seventeenlands_key]
+    card.update(cattrs.unstructure(stats))
+    return card
 
 
-@define
-class CardWithStats:
-    # colors: List[str] = attrs.field(default=attrs.Factory(list))
-    seen_count: int
-    avg_seen: float
-    pick_count: int
-    game_count: int
-    win_rate: float
-    sideboard_game_count: int
-    sideboard_win_rate: float
-    drawn_game_count: int
-    drawn_win_rate: float
-    ever_drawn_game_count: int
-    drawn_game_count: int
-    drawn_win_rate: float
-    ever_drawn_game_count: int
-    ever_drawn_win_rate: float
-    never_drawn_game_count: int
-    never_drawn_win_rate: float
-    drawn_improvement_win_rate: float
+def _process_raw_cards():
+
+    # make a custom cattrs "structure" function that renames fields according to field aliases
+    rename = {}
+
+    for field in attrs.fields(Card):
+        if "alias" in field.metadata:
+            rename[field.name] = override(rename=field.metadata["alias"])
+
+    fromdict = make_dict_structure_fn(Card, cattrs.Converter(), **rename)
+
+    todict = make_dict_unstructure_fn(Card, cattrs.Converter())
+
+    path = cached_path(
+        RAW_DATA_URL,
+        cache_dir=MTGLEARN_CACHE_HOME,
+        ignore_url_params=True,
+        use_etag=False,
+    )
+
+    raw_dataset = defaultdict(list)
+
+    with open(path) as f:
+        raw_data = json.load(f)["data"]
+
+    for expansion_name, expansion_data in raw_data.items():
+        for raw_card in expansion_data["cards"]:
+            raw_card["expansion"] = expansion_name
+            card = fromdict(raw_card)
+            for k, v in todict(card).items():
+                raw_dataset[k].append(v)
+
+    dataset = Dataset.from_dict(raw_dataset, features=type2features(Card))
+
+    dataset.save_to_disk(CARDS_DATASET_CACHE)
+
+    return dataset
 
 
-EXPANSIONS = ["VOW"]
-
-
-def load_cards_with_stats():
-
-    cards = load_cards()
-
-    for expansion in EXPANSIONS:
-        expansion_cards = cards.filter(lambda x: x["expansion"] == expansion)
-        print(expansion_cards)
-        break
-
-        endpoint = f"https://www.17lands.com/card_ratings/data?expansion={expansion}&format=PremierDraft"
-        raw_data = requests.get(endpoint).json()
+def _try_load(filename: str) -> Optional[Dataset]:
+    if os.path.exists(CARDS_DATASET_CACHE):
+        try:
+            return load_from_disk(CARDS_DATASET_CACHE)
+        except Exception as e:
+            logger.error(f"could not load dataset from {CARDS_DATASET_CACHE}: {e}")
+    return None
 
 
 def load_cards(
-    as_attrs=False, as_dataset=False, as_dataframe=False, shuffle=False, seed=None
+    as_dataset=False,
+    as_attrs=False,
+    as_dataframe=False,
+    with_stats=False,
 ):
+
+    if sum([as_attrs, as_dataframe, as_dataset]) > 1:
+        raise ValueError(
+            "Only one of 'as_attrs', 'as_dataframe', or 'as_dataste' must be set."
+        )
 
     # as_dataframe is the default
     if not (as_attrs or as_dataset):
         as_dataframe = True
 
-    path = cached_path(RAW_DATA_URL)
+    dataset = _try_load(CARDS_DATASET_CACHE)
 
-    cards = []
+    if dataset is None:
+        dataset = _process_raw_cards()
 
-    with open(path) as f:
-        raw_data = json.load(f)["data"]
-        for expansion_name, expansion_data in raw_data.items():
-            for raw_card in expansion_data["cards"]:
-                cards.append(Card.from_dict(raw_card, expansion=expansion_name))
+    if with_stats:
 
-    if shuffle:
-        random.Random(seed).shuffle(cards)
+        card_stats = _try_load(CARD_STATS_DATASET_CACHE)
 
-    if as_attrs:
-        return cards
+        if card_stats is None:
+            # filter out cards that won't have stats
+            dataset = dataset.filter(lambda c: c["expansion"] in EXPANSIONS_WITH_STATS)
+            dataset = dataset.filter(lambda c: c["name"] not in BASIC_LANDS)
+            card_stats = dataset.map(
+                _join_card_with_stats, features=type2features(Card, CardStats)
+            )
+            card_stats.save_to_disk(CARD_STATS_DATASET_CACHE)
 
-    dataset = defaultdict(list)
+        dataset = card_stats
+
+    if as_dataset:
+        return dataset
+
+    todict = make_dict_unstructure_fn(Card, cattrs.Converter())
 
     for card in cards:
-        for k, v in cattrs.unstructure(card).items():
+        for k, v in todict(card).items():
             dataset[k].append(v)
 
-    features = cls2features(Card)
+    features = type2features(Card)
 
     dataset = Dataset.from_dict(dataset, features=features)
 
@@ -144,7 +157,3 @@ def load_cards(
 
     # as_dataset=True
     return dataset
-
-
-if __name__ == "__main__":
-    load_cards_with_stats()
---
+++ src/mtglearn/datasets/utils.py
@@ -0,0 +1,40 @@
+from typing import List, Optional, Set
+from datasets import Features, Value, Sequence
+import attrs
+
+
+def type2features(*types) -> Features:
+    """
+    A helper function for turning an attrs class into the corresponding dataset.Features object
+    """
+
+    features = {}
+
+    for cls in types:
+
+        # if Optional grab actual type
+        if type(cls) is type(Optional[str]):
+            cls = cls.__args__[0]
+
+        # if list, sequence of inner type
+        if type(cls) is type(List[str]):
+            return Sequence(type2features(cls.__args__[0]))
+
+        # scalars
+        if cls is str:
+            return Value("string")
+        if cls is int:
+            return Value("int32")
+        if cls is float:
+            return Value("float32")
+
+        # if is an attrs class, recurse and update
+        if attrs.has(cls):
+            for field in attrs.fields(cls):
+                field_feature = type2features(field.type)
+                features[field.name] = field_feature
+
+    if not features:
+        raise NotImplementedError(str(types))
+
+    return Features(**features)
